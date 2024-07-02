43b0df7089ee03b4cf829f006f9e3d61cd62b8b3
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2024-07-03 01:09:51 +0300

    added build

---
+++ build/precache-manifest.79300912b05adf65a21475e9b2f062f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370dcd3cde1256ac881e485c0fcabd34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52743e5204ab2702139d",
+    "url": "/static/css/main.18924e37.chunk.css"
+  },
+  {
+    "revision": "fdc612a5c0e1279cfd0d",
+    "url": "/static/js/2.4234b5d7.chunk.js"
+  },
+  {
+    "revision": "3395fd58c601018bb36cf426538ccd9f",
+    "url": "/static/js/2.4234b5d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47f5b629f9198a09fce",
+    "url": "/static/js/3.937dc1a2.chunk.js"
+  },
+  {
+    "revision": "52743e5204ab2702139d",
+    "url": "/static/js/main.0313324f.chunk.js"
+  },
+  {
+    "revision": "3276c07ee70463cb445b",
+    "url": "/static/js/runtime-main.08d4c63e.js"
+  }
+]);
